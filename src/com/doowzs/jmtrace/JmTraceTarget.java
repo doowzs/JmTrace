e73d73bf06722c2bd8847f16fd4297777a6d29d3
Tianyun Zhang <doowzs@qq.com>
2020-11-26 13:58:52 +0800

    add  xALOAD / xASTORE instructions

--- src/com/doowzs/jmtrace/JmTraceTarget.java
+++ src/com/doowzs/jmtrace/JmTraceTarget.java
@@ -31,8 +31,20 @@ public class JmTraceTarget {
     public static JmTraceTarget[] targets = new JmTraceTarget[] {
         new JmTraceTarget(Opcodes.AALOAD, true, Type.getType("[Ljava/lang/Object;")),
         new JmTraceTarget(Opcodes.AASTORE, true, Type.getType("[Ljava/lang/Object;"), Opcodes.ALOAD, Opcodes.ASTORE),
+        new JmTraceTarget(Opcodes.BALOAD, true, Type.getType("[B")), // byte or boolean
+        new JmTraceTarget(Opcodes.BASTORE, true, Type.getType("[B"), Opcodes.ILOAD, Opcodes.ISTORE),
+        new JmTraceTarget(Opcodes.CALOAD, true, Type.getType("[C")),
+        new JmTraceTarget(Opcodes.CASTORE, true, Type.getType("[C"), Opcodes.ILOAD, Opcodes.ISTORE),
+        new JmTraceTarget(Opcodes.DALOAD, true, Type.getType("[D")),
+        new JmTraceTarget(Opcodes.DASTORE, true, Type.getType("[D"), Opcodes.DLOAD, Opcodes.DSTORE),
+        new JmTraceTarget(Opcodes.FALOAD, true, Type.getType("[F")),
+        new JmTraceTarget(Opcodes.FASTORE, true, Type.getType("[F"), Opcodes.FLOAD, Opcodes.FSTORE),
         new JmTraceTarget(Opcodes.IALOAD, true, Type.getType("[I")),
-        new JmTraceTarget(Opcodes.IASTORE, true, Type.getType("[I"), Opcodes.ILOAD, Opcodes.ISTORE)
-        // TODO: Add opcodes like xALOAD, xASTORE
+        new JmTraceTarget(Opcodes.IASTORE, true, Type.getType("[I"), Opcodes.ILOAD, Opcodes.ISTORE),
+        new JmTraceTarget(Opcodes.LALOAD, true, Type.getType("[J")),
+        new JmTraceTarget(Opcodes.LASTORE, true, Type.getType("[J"), Opcodes.LLOAD, Opcodes.LSTORE),
+        new JmTraceTarget(Opcodes.SALOAD, true, Type.getType("[S")),
+        new JmTraceTarget(Opcodes.SASTORE, true, Type.getType("[S"), Opcodes.ILOAD, Opcodes.ISTORE)
+        // TODO: Add opcodes like GETSTATIC, PUTSTATIC
     };
 }
--- tests/P1001/Main.java
+++ tests/P1001/Main.java
@@ -3,10 +3,10 @@ import java.util.*;
 public class Main {
     public static void main(String[] args) {
         Scanner cin = new Scanner(System.in);
-        Integer a[] = new Integer[3];
-        a[0] = cin.nextInt();
-        a[1] = cin.nextInt();
-        a[2] = a[0] + a[1];
+        short a[] = new short[3];
+        a[0] = cin.nextShort();
+        a[1] = cin.nextShort();
+        a[2] = (short)(a[0] + a[1]);
         System.out.println(a[2]);
     }
 }
